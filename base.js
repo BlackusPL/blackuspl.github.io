136da939d8a0e450fa7bd6c7000a962f877d077d
BlackusPL <45206560+BlackusPL@users.noreply.github.com>
2023-03-08 10:07:54 +0100

    profile position fix

--- base.js
+++ base.js
@@ -196,7 +196,7 @@ const DA = {
     console.log("%cVersion: %c1.3" , "color: rgb(58, 113, 193)","color: gray;")
   }
 };
-document.querySelector("footer").insertAdjacentHTML('beforeend', `<div id="txt" style="display: inline-block;position: absolute;right: 14%;"></div>`);
+document.querySelector("footer").insertAdjacentHTML('beforeend', `<div id="txt" style="display: inline-block; float: right; margin-right: 1.5em;"></div>`);
 startTime();
 function startTime() {
   const today = new Date(Date.now());
