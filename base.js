87b93d13aa46478cf07e2c173148cb3fdb0e4f0b
BlackusPL <thejaycraft29j@gmail.com>
2023-04-13 23:25:47 +0200

    fix for latest update

--- base.js
+++ base.js
@@ -5,10 +5,10 @@ function jezykoff() {
 }
 
 
-function jezyk() {
+function jezyk(lang) {
   var data = new Date();
   data.setDate(data.getDate() + 3);
-  document.cookie = ("language=pl;path=/;expires=" + data);
+  document.cookie = (`language=${lang};path=/;expires=` + data);
   //localStorage.setItem('language', 'pl');
   window.location.reload();
 }
@@ -42,7 +42,8 @@ getID("tab").insertAdjacentHTML('afterend', `<ul id="tab" class="notranslate">
 <div class="dropdown">
 <button class="dropbtn">Languages ↡ </button>
 <div class="showtasks">
-  <a onclick="jezyk()">Polish</a>
+  <a onclick="jezyk('pl')">Polish</a>
+  <a onclick="jezyk('de')">German</a>
   <a onclick="jezykoff()">English</a>
 </div></div>
 <a href="https://cse.google.com/cse?cx=f57e79ca12bca49ec">Search</a>
@@ -55,103 +56,28 @@ getID("tab").remove();*/
     var finded = document.cookie.split("; ").find((lang) => lang.startsWith(`${coname}=`))?.split("=")[1];
     return finded;
   }
-  if (cookievaluefinder('language') == "pl")  {
+  if (window.location.href.split('/')[3] == "DarknessAir") {var web_root = `${window.location.host}/DarknessAir`} else {var web_root = window.location.host}
   var zmien, regex, key, textnodes, node, s;
 
-  zmien = {
-    // Here is for ChangeLog translation
-    'Remake of navigation bar': 'Przebudowa paska nawigacyjnego',
-    'Added Search Button to able search more information about games': 'Dodano przycisk wyszukiwania, aby móc wyszukiwać więcej informacji o grach.',
-    'Translation soon on Games pages': 'Tłumaczenie do stron z grami wkrótce',
-    'Updated icon and images on website': 'Zaaktualizowano ikonę i obraz strony',
-    'Changed text on Main page': 'Zmieniono główny tekst na stronie głównej',
-    'Updated css code': 'Zaaktualizowano kod CSS',
-    'Added "Last Updated" text on': 'Dodano "Ostatnio Aktualizowane" na',
-    'Everything about me and website in video below': 'Wszystko o mnie i o stronie na filmie poniżej',
-    'Optimized and fixed some code from': 'Zoptymalizowano i naprawiono kod na',
-    'Added "About Game" for': 'Dodano "O Grze" na',
-    'Added Changelog list': 'Dodano listę zmian',
-    'Updated code to': 'Zaaktualizowano kod do',
-    'doesnt exist in HTML5 so i fixed it': 'nie istnieje w HTML5 więc to naprawiłem',
-    'Added option to change language on website': 'Dodano opcje by zmienić język strony',
-    'Required cookies enabled': 'Wymaga włączonych ciasteczek',
-    'Changed entired': 'Zmieniono całą',
-    'Radio Page': 'Stronę Odtwarzacza',
-    'Now website has a preview of links on other websites': 'Teraz strona ma podgląd linków na innych stronach',
-    'Added Button to set custom background and reset it to default on': 'Dodano opcję ustawienia własnego tła strony i jego zresetowania na',
-    'Added more songs in': 'Dodano więcej piosenek na',
-    'Added Music Player, Discord widget on Home page and first tests with javascripts': 'Dodano Odtwarzacz, Widżet Discorda na Stronie głównej i pierwsze testy z JavaScript',
-    'Added Cursor and About me page': 'Dodano kursor i O mnie stronę',
-    'Fixed Website URL': 'Naprawiono adres strony',
-    'Added Translation to all Games Pages': 'Dodano tłumaczenie do strony z grami',
-    'Uses Google API but doesnt work': 'Użyto Google API lecz nie działa',
-    'Added song and radio station': 'Dodano piosenkę i stacje radiową',
-    'Button Pages was changed to Games' : 'Przycisk Strony został zmieniony na Gry',
-    'Added News page where you can find changelog about website and games' : 'Dodano stronę Nowości gdzie możecie znaleźć dziennik zmian strony i gier',
-    'Updated Music Racer 2000 page' : 'Zaaktualizowano stronę Music Racer 2000',
-    'Added new project in about page' : 'Dodano nowy projekt na stronie o mnie',
-    'Added my avatar in' : 'Dodano mój awatar na',
-    'Updated Webamp to newest version' : 'Zaaktualizowano Webamp do najnowszej wersji',
-    'You can now exit from Music Player when you dont have javascripts' : 'Możesz teraz wyjść z odtwarzacza muzyki, gdy nie masz javascripts',
-    'Fixed songs and added new one' : 'Naprawiono piosenki i dodano nowe',
-    'Fixed links to author profile' : 'Naprawiono linki do profilu autora',
-    'The download link for the game from Steam has been changed to Widget' : 'Link do pobrania gry z Steam został zmieniony na Widget',
-    'If you have suggestions on what to add, write to me' : 'Jak masz propozycje co mam dodać to pisz do mnie',
-    'Added some new songs' : 'Dodano kilka nowych piosenek',
-    'Added milkdrop to Webamp' : 'Dodano milkdrop do Webampa',
-    'Fixed the Home button being able to exit the website' : 'Naprawiono przycisk Home, którym można było wyjść ze strony',
-    'Fixed Context Menu in Webamp' : 'Naprawiono Menu kontekstowe w Webampie',
-    'Changelog will be moved to News soon' : 'Changelog zostanie wkrótce przeniesiony do News',
-    'Changed entire News page' : 'Zmieniono całą stronę Nowości',
-    'Added animated background' : 'Dodano animowane tło',
-    'The newest Coub will now be played in full hd' : 'Najnowszy Coub będzie teraz odtwarzany w pełnej rozdzielczości HD',
-    'Discord widget now have normal position' : 'Widżet Discord ma teraz normalną pozycję',
-    'Fixed some things in CSS' : 'Naprawiono kilka rzeczy w CSS',
-    'Again next testing google translator' : 'Znowu kolejne testy z google translatorem',
-    'Changed entire Games page': 'Zmieniono całą stronę Gry',
-    'You can now Download my plugins on' : 'Możesz teraz pobrać moje pluginy na',
-    'Updated console text' : 'Zaktualizowany tekst konsoli',
-    'New look on My Project in about me page' : 'Nowy wygląd moich Projektów na stronie o mnie',
-    'Video background in now untouchable' : 'Tło jako film jest teraz nietykalne',
-    'If you dont have JavaScript on, you get notification about this' : 'Jeżeli nie posiadasz właczonych JavaScript, to dostaniesz komunikat o tym',
-    // The end of ChangeLog
-    'Music Player': 'Odtwarzacz',
-    'Choose playlist' : 'Wybierz playliste',
-    'Select Playlist' : 'Wybierz Playliste',
-    'All' : 'Wszystkie',
-    'songs' : 'piosenki',
-    'Social Links': 'Linki',
-    'Games': 'Gry',
-    'About Me': 'O mnie',
-    'AI Chat' : 'Czat SI',
-    'Usefull stuffs' : 'Przydatne rzeczy',
-    'News' : 'Nowości',
-    'Languages': 'Języki',
-    'Search': 'Wyszukaj',
-    'Polish': 'Polski',
-    'English': 'Angielski',
-    'Abandoned': 'Porzucone',
-    'October': 'Października',
-    'August': 'Sierpnia',
-    'January': 'Stycznia',
-    'ChangeLog': 'Dziennik zmian',
-    'My Projects': 'Moje Projekty',
-    'Set custom background' : 'Ustaw własne tło',
-    'Last Updated': 'Ostatnio Aktualizowane',
-    'All rights reserved': 'Wszelkie prawa zastrzeżone',
-    'DO NOT DISTRIBUTE': 'NIE ROZPOWSZECHNIAĆ',
-    'More comming soon...': 'Więcej wkrótce...',
-    'Everything about me i website in video below': 'Wszystko o mnie i stronie w filmie poniżej',
-    'Click here': 'Kliknij tutaj',
-    'to be redirected to my Coub': 'by zostać przeniesiony na mojego Couba',
-    'If redirect not working go to ': 'Jeżeli klikłeś i nic przejdź na ',
-    'Website was created': 'Strona została stworzona',
-    'and': 'i',
-    'Latest Coub' : 'Najnowszy Coub',
-    ///////////////////////////////////////////////////////
-    '': ''
-  };
-
+  switch(cookievaluefinder('language')) {
+    case 'pl':
+  fetch(`https://${web_root}/languages/pl.json`)
+  .then(response => response.json())
+  .then(data => {
+    zmien = data;
+    SetZmien();
+  });
+      break;
+    case 'de':
+    fetch(`https://${web_root}/languages/de.json`)
+    .then(response => response.json())
+    .then(data => {
+      zmien = data;
+      SetZmien();
+    });
+      break;
+  }
+  function SetZmien() {
   regex = {};
   for (key in zmien) {
     regex[key] = new RegExp(key, 'g');
