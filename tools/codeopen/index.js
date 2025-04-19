e5888d5f923ff43febe5cf740ba1db784222b897
BlackusPL <thejaycraft29j@gmail.com>
2025-04-19 21:59:52 +0200

    Update 10.04.2025 This is a big update for Code Opener - Changed boring textarea to monaco editor to make syntax highlighting and ability to format codeopen - Changed Translator to Markdown viewer because who even wants to use Translator in cheap way - Removed Session saving code, but this will be readded soon - Fixed issue where in DataURi and LocalPlayer, file upload apears in the wrong place

--- tools/codeopen/index.js
+++ tools/codeopen/index.js
@@ -1,25 +1,15 @@
 (() => {
 const id = (_) => document.getElementById(_);
 // gets value for head and body from session
-id('body_code').value = sessionStorage.getItem('body_code');
-id('head_code').value = sessionStorage.getItem('head_code');
-// if value in body_code or head changed, then add to session storage values
-id('body_code').addEventListener('change', function () {
-   sessionStorage.setItem('body_code', `${id('body_code').value}`);
-});
-id('head_code').addEventListener('change', function () {
-    sessionStorage.setItem('head_code', `${id('head_code').value}`);
-});
-// cwhen click then clear sessionstorage and values in head and body
-id('clear').addEventListener('click', function () {
-    sessionStorage.removeItem('body_code');
-    sessionStorage.removeItem('head_code');
-    try { id('head_code').value = null } catch {};
-    try { id('body_code').value = null } catch {};
-});
-function rm_html() {
-    try {id('afterselectcon').remove(); id('video_src').remove()} catch {}
-};
+// id('body_code').value = sessionStorage.getItem('body_code');
+// id('head_code').value = sessionStorage.getItem('head_code');
+// // if value in body_code or head changed, then add to session storage values
+// id('body_code').addEventListener('change', function () {
+//    sessionStorage.setItem('body_code', `${id('body_code').value}`);
+// });
+// id('head_code').addEventListener('change', function () {
+//     sessionStorage.setItem('head_code', `${id('head_code').value}`);
+// });
 // embed video from selected file
 function playVideo(files) {
     try {
@@ -27,112 +17,115 @@ function playVideo(files) {
       id('video_src').src = URL.createObjectURL(files[0]);
     } catch (e) {}
 };
-
-    document.querySelector("#afterselectcon ~ button + button").addEventListener('click', function () {
-            switch (document.querySelector('center select').value) {
-                default:
-                    alert('WTH you select?')
-                break;
-                case 'HTML':
-                    if (id('afterselect').checked) {
-                        window.open().document.write(`${id('body_code').value}`)
-                    } else {
-                        window.open().document.write(`<!DOCTYPE html><html><head>${id('head_code').value}</head><body>${id('body_code').value}</body></html>`)
-                    }
-                break;
-                case 'JSON':
-                    try{window.open(window.URL.createObjectURL(
-                        new Blob([`${JSON.stringify(JSON.parse([id('body_code').value]), undefined, 2)}`], {type: "application/json"})
-                        )
-                    )} catch(error) {console.error(error); alert(error)}
-                break;
-                case 'JS':
-                    //function evalFunc(_copy = false, showResult = false) {
-                    function evalFunc() {
-                        try {
-                          const result = eval(id('head_code').value);
-                          //if (showResult) {
-                            id('body_code').value = result;
-                            //return;
-                          //}
-                          //_copy && copyValue(result);
-                        } catch (error) {
-                          alert(error);
-                        }
-                    }
-                    evalFunc()
-                break;
-                case 'Translator':
-                    alert('soon')
-                break;
-                case 'Localplayer':
-                    window.open().document.write(`<video style="width: 100%; height: 100%;" src="${id('video_src').src}" controls></video>`)
-                break;
-                case 'DataURI':
-                    switch (id('body_code').value.substring(0, 10)) {
-                        case 'data:audio':
-                            window.open().document.write(`<video controls autoplay name="media"><source src="${id('body_code').value}"></video>`)
-                        break;
-                        case 'data:video':
-                            window.open().document.write(`<video controls autoplay name="media" style="width: 100%; height:100%;"><source src="${id('body_code').value}"></video>`)
-                        break;
-                        case 'data:image':
-                            window.open().document.write(`<image src="${id('body_code').value}">`)
-                        break;
-                        default:
-                            window.open().document.write(`${id('body_code').value}`)
-                        break;
-                    }
-                break;
-                case 'Google It':
-                    window.open(`https://letmegooglethat.com/?q=${id('head_code').value}`, '_blank')
-                break;
-                case 'soon':
-                    alert('soon')
-                break;
-            }
+require.config({
+    paths: {
+      vs: "https://cdn.jsdelivr.net/npm/monaco-editor@0.52.2/min/vs",
+    },
+  }),
+    require(["vs/editor/editor.main"], () => {
+        var th = monaco.editor.create(id("head_code"), {
+            //value: "<!--This is head code-->",
+            language: "html",
+            theme: 'vs-dark',
+            wordWrap: "on",
+            placeholder: "This is head code",
+            "autoIndent": true,
+            "formatOnPaste": true,
+            "formatOnType": true
         });
-    document.querySelector('center select').addEventListener('change', function () {
-        switch (document.querySelector('center select').value) {
+        var tb = monaco.editor.create(id("body_code"), {
+          value: "",
+          language: "html",
+          theme: 'vs-dark',
+          wordWrap: "on",
+          placeholder: "Elements that will be in body or html code",
+          "autoIndent": true,
+          "formatOnPaste": true,
+          "formatOnType": true
+        });
+        // // cwhen click then clear sessionstorage and values in head and body
+        id('clear').addEventListener('click', function () {
+            //     sessionStorage.removeItem('body_code');
+            //     sessionStorage.removeItem('head_code');
+            th.setValue("")
+            tb.setValue("");
+        });
+        var selector = document.querySelector('#codeplace ~ div select');
+        var submit = document.querySelector("#afterselectcon ~ button + button");
+        //t.setValue(document.querySelector("pre").innerText);
+        //document.querySelector("pre").remove();
+        function choosen() {
+          const showhide = (e, n) => {
+            let i;
+            switch (e) {
+                case 0:
+                    i = 'afterselectcon';
+                    break;
+                case 1:
+                    i = 'head_code';
+                    break;
+                case 2:
+                    i = 'body_code';
+                    break;
+            }
+            return id(i).style.display = Boolean(n) ? 'block' : 'none';
+          }
+          try {document.getElementById('beforecheckbox').remove(); id('video_src').remove(); id('vidframe').remove()} catch {};
+          zmien(selector.value.toLowerCase());
+          // Selection Menu
+          switch (selector.value) {
             default:
-                rm_html()
-                id("codeplace").innerHTML = `<textarea placeholder="Soon" id="head_code"></textarea><br> \
-              <textarea placeholder="Soon" id="body_code"></textarea>`;
+                tb.setValue("")
+                tb.updateOptions({ placeholder: "Soon", readOnly: true });
+                showhide(1,0);
+                showhide(2,1);
+                showhide(0,0);
                 console.log('File type not found')
                 break;
             case 'HTML':
-                rm_html()
-                id("codeplace").innerHTML = `<textarea placeholder="Elements in head" id="head_code"></textarea><br> \
-              <textarea placeholder="Elements that will be in body or html code" id="body_code"></textarea>`;
-                document.querySelector('select').insertAdjacentHTML('afterend','<div id="afterselectcon"><input type="checkbox" id="afterselect">Full HTML</input></div>')
-                id('afterselect').addEventListener('change', function () {
-                    if (id('afterselect').checked) {
-                        id('head_code').style.display = "none";
-                    } else {
-                    id('head_code').style.display = ""
-                }
-            })
-            break;
+                showhide(0,1);
+                showhide(1,1);
+                showhide(2,1);
+                th.setValue("")
+                tb.setValue("")
+                th.updateOptions({ placeholder: "This is head code" });
+                tb.updateOptions({ placeholder: "Elements that will be in body or html code", readOnly: false });
+                break;
             case 'JSON':
-                rm_html()
-                id("codeplace").innerHTML = `<textarea placeholder="JSON code to be formatted" id="body_code"></textarea>`;
-            break;
-            case 'JS':
-                rm_html()
-                id("codeplace").innerHTML = `<textarea placeholder="Code to eval" id="head_code"></textarea><br> \
-                <textarea placeholder="Result" id="body_code"></textarea>`;
-            break;
-            case 'Translator':
-                rm_html()
-                id("codeplace").innerHTML = `<textarea placeholder="Text to translate" id="head_code"></textarea><br> \
-                <textarea placeholder="Translated text" id="body_code"></textarea>`;
-            break;
+                tb.setValue("{\n    \"_comment\" : \"JSON code to be formatted\"\n}");
+                tb.updateOptions({ placeholder: "JSON code to be formatted", readOnly: false });
+                showhide(1,0);
+                showhide(2,1);
+                showhide(0,0);
+                // Usefull for next project
+                setTimeout(() => {
+                    tb.getAction('editor.action.formatDocument').run();
+                }, 500)
+                break;
+            case 'JavaScript':
+                th.setValue("")
+                th.updateOptions({ placeholder: "/* Code to eval */" });
+                tb.setValue("");
+                tb.updateOptions({ placeholder: "Result", readOnly: true });
+                showhide(1,1);
+                showhide(2,1);
+                showhide(0,0);
+                break;
+            case 'Markdown':
+                tb.setValue("")
+                tb.updateOptions({ placeholder: "Here goes markdown" });
+                showhide(1,0);
+                showhide(2,1);
+                showhide(0,0);
+                break;
             case 'Localplayer':
-                rm_html()
-                document.querySelector('select').insertAdjacentHTML('afterend','<div id="afterselectcon"><input type="file" accept="video/*" id="afterselect"></input></div>');
-                document.querySelector('#codeplace').innerHTML = `<video id="video_src" controls></video><br> \
-                <input type="number" name="width"> Width \
-                <input type="number" name="height"> Height`;
+                showhide(0,0);
+                showhide(1,0);
+                showhide(2,0);
+                document.querySelector('#codeplace ~ div select').insertAdjacentHTML('afterend','<div id="beforecheckbox"><input type="file" accept="video/*" id="afterselect"></input></div>');
+                document.querySelector('#codeplace').insertAdjacentHTML('beforeend', `<div id="vidframe"><video id="video_src" controls></video><br>
+                <input type="number" name="width"> Width
+                <input type="number" name="height"> Height</div>`);
                 id('afterselect').addEventListener('change', function () {
                     playVideo(this.files)
                 });
@@ -142,31 +135,101 @@ function playVideo(files) {
                 document.querySelector('[name=height').addEventListener("change", function () {
                     id('video_src').style.height = `${document.querySelector('[name=height]').value}px`
                 });
-            break;
+                break;
             case 'DataURI':
-                rm_html();
-                id("codeplace").innerHTML = `<textarea placeholder="Result" id="body_code"></textarea>`;
-                document.querySelector('select').insertAdjacentHTML('afterend','<div id="afterselectcon"><input type="file" accept="*/*" id="afterselect"></input></div>');
+                showhide(0,0);
+                showhide(1,0);
+                showhide(2,1);
+                tb.setValue("")
+                tb.updateOptions({ placeholder: "Result", readOnly: true });
+                document.querySelector('#codeplace ~ div select').insertAdjacentHTML('afterend','<div id="beforecheckbox"><input type="file" accept="*/*" id="afterselect"></input></div>');
                 id('afterselect').addEventListener('change', function () {
-                function blobToDataURL(blob) {
-                    return new Promise((res) => {
-                      var a = new FileReader();
-                      a.onload = () => res(a.result);
-                      a.readAsDataURL(blob);
-                    });
-                };
-                async function dataURLFunc(text) {
-                    id("body_code").value = await blobToDataURL(text);
-                };
-                dataURLFunc(this.files[0]);
-            });
-            break;
-            case 'Google It':
-                rm_html();
-                id("codeplace").innerHTML = `<textarea placeholder="Type what you want to search for" id="head_code"></textarea>`;
+                    function blobToDataURL(blob) {
+                        return new Promise((res) => {
+                          var a = new FileReader();
+                          a.onload = () => res(a.result);
+                          a.readAsDataURL(blob);
+                        });
+                    };
+                    async function dataURLFunc(text) {
+                        tb.setValue(await blobToDataURL(text))
+                    };
+                    dataURLFunc(this.files[0]);
+                });
+                break;
+            case "Google It":
+                showhide(0,0);
+                showhide(1,0);
+                showhide(2,1);
+                tb.setValue("")
+                tb.updateOptions({ placeholder: "Type what you want to search for", language: "plaintext", readOnly: false });
+            };
         }
-    })
-
-console.log("%c[NewPage Scripts] %cSuccesfully Loaded","color: rgb(58, 113, 193)","color: white;")
-// window.open().document.write(`<pre>${JSON.stringify(othersong)}</pre>`)
+        // function to set language in editors
+        function zmien(_) {
+            monaco.editor.setModelLanguage(tb.getModel(), _);
+            monaco.editor.setModelLanguage(th.getModel(), _);
+        }
+        // Run Code
+        function odpal() {
+            switch (selector.value) {
+                default:
+                    alert('WTH you select?')
+                break;
+                case 'HTML':
+                    var head = th.getValue();
+                    var body = tb.getValue();
+                    var fullhtml = id('afterselect').checked ? `<html><head>${head}</head><body>${body}</body></html>` : `${head}${body}`;
+                    window.open().document.write(fullhtml);
+                break;
+                case 'JSON':
+                    var fulljson = JSON.parse(tb.getValue());
+                    window.open().document.write(JSON.stringify(fulljson, null, 2));
+                break;
+                case 'JavaScript':
+                    try {
+                        const result = eval(th.getValue());
+                        tb.setValue(result);
+                    } catch (error) {
+                        tb.setValue(error.toString());
+                    }
+                break;
+                case 'Markdown':
+                    window.open().document.write(`<body><script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script><script>document.body.innerHTML = marked.parse(\`${tb.getValue()}\`);</script></body>`)
+                break;
+                case 'Localplayer':
+                    window.open().document.write(`<video style="width: 100%; height: 100%;" src="${id('video_src').src}" controls></video>`)
+                break;
+                case 'DataURI':
+                    switch (tb.getValue().substring(0, 10)) {
+                        case 'data:audio':
+                            window.open().document.write(`<video controls autoplay name="media"><source src="${tb.getValue()}"></video>`)
+                        break;
+                        case 'data:video':
+                            window.open().document.write(`<video controls autoplay name="media" style="width: 100%; height:100%;"><source src="${tb.getValue()}"></video>`)
+                        break;
+                        case 'data:image':
+                            window.open().document.write(`<image src="${tb.getValue()}">`)
+                        break;
+                        default:
+                            window.open().document.write(`${tb.getValue()}`)
+                        break;
+                    }
+                break;
+                case 'Google It':
+                    window.open(`https://letmegooglethat.com/?q=${tb.getValue()}`, '_blank')
+                break;
+                case 'soon':
+                    alert('soon')
+                break;
+            };
+        };
+        selector.onchange = choosen;
+        submit.onclick = odpal;
+        // document.querySelectorAll("[class=\"view-lines monaco-mouse-cursor-text\"]").forEach((e) => {
+        //   e['style'].width = null;
+        // });    
+    });
+    console.log("%c[NewPage Scripts] %cSuccesfully Loaded","color: rgb(58, 113, 193)","color: white;")
+    // window.open().document.write(`<pre>${JSON.stringify(othersong)}</pre>`)
 })();
